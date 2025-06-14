f40be21d90249dd94c818c001d5584786fc51857
Akhil-A0402 <akhil.a.nair760@gmail.com>
2025-06-14 12:59:11 +0530

    url trim init

---
+++ urltrim/src/main/java/com/reliablelink/urltrim/UrlTrimmerApplication.java
@@ -0,0 +1,13 @@
+package com.reliablelink.urltrim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlTrimmerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlTrimmerApplication.class, args);
+	}
+
+}
